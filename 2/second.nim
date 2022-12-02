9fc8e5e7de785d70eb39db240cf02b37a3ad5bf6
Duskhorn <uwueclipsedhorn@gmail.com>
2022-12-02 14:16:32 +0100

    Advent of Code - Day 2

--- 2/second.nim
+++ 2/second.nim
@@ -2,6 +2,7 @@ import strutils
 
 let content = readFile("input")
 
+## part 1
 proc convert(c: char): int =
     case c
     of 'A', 'X':
@@ -10,7 +11,7 @@ proc convert(c: char): int =
         return 2
     of 'C', 'Z':
         return 3
-    else: return -10
+    else: discard
 
 var res = 0
 for line in content.splitLines():
@@ -26,3 +27,46 @@ for line in content.splitLines():
         res += 6
 
 echo res
+
+## part 2
+type Ending = enum 
+    Loss,
+    Draw,
+    Win
+
+proc points(val: int, ending: Ending): int =
+    result += val + 3 * ending.int  
+
+
+proc output(c: char): Ending =
+    case c
+    of 'X': result = Loss
+    of 'Y': result = Draw
+    of 'Z': result = Win
+    else: discard
+
+var res2 = 0
+for line in content.splitLines():
+    let 
+        opp = line[0].convert
+        strat = line[2].output
+    
+    let elf = case strat 
+        of Draw: 
+            opp
+        of Loss: 
+            case opp
+            of 1: 3
+            of 2: 1
+            of 3: 2
+            else: 0
+        of Win:
+            case opp:
+            of 1: 2
+            of 2: 3
+            of 3: 1
+            else: 0
+    
+    res2 += points(elf, strat)
+
+echo res2
